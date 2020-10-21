9d1ed669335d89155f204b9a5382a24f8426e1b0
Matt Turner <matt@fiverings.uk>
2020-10-21 12:53:04 +0100

    Installed IR sensor

--- massive-octo-tribble.ino
+++ massive-octo-tribble.ino
@@ -1,37 +1,144 @@
-int ledRedPin = 13;
-int ledGreenPin = 12;
-int motorLeftForwardPin = 9;
-int motorLeftReversePin = 10;
-int motorRightForwardPin = 7;
-int motorRightReversePin = 8;
-int inPin = 2;
-int val = 0;
+#include <L293.h>
+#include <IRremote.h>
+
+int NUMBER_OF_LEDS = 3;
+int ledRedPin = 12;
+int ledGreenPin = 10;
+int ledBluePin = 11;
+int leds[] = { ledGreenPin, ledRedPin, ledBluePin };
+
+int irSensorPin = 4;
+int ledOnboardPin = 13;
+int motorLeftEnablePin = 9;
+int motorLeftForwardPin = 7;
+int motorLeftReversePin = 8;
+int motorRightEnablePin = 3;
+int motorRightForwardPin = 6;
+int motorRightReversePin = 5;
+
+L293 motorLeft(motorLeftEnablePin, motorLeftForwardPin, motorLeftReversePin);
+L293 motorRight(motorRightEnablePin, motorRightForwardPin, motorRightReversePin);
+
+IRrecv irrecv(irSensorPin);
+decode_results results;
+
+bool isMoving = false;
 
 void setup() {
+  Serial.begin(9600);
+  
   pinMode(ledRedPin, OUTPUT);
   pinMode(ledGreenPin, OUTPUT);
-  pinMode(motorLeftForwardPin, OUTPUT);
-  pinMode(motorLeftReversePin, OUTPUT);
-  pinMode(motorRightForwardPin, OUTPUT);
-  pinMode(motorRightReversePin, OUTPUT);
-  pinMode(inPin, INPUT);
+  pinMode(ledBluePin, OUTPUT);
+  pinMode(ledOnboardPin, OUTPUT);
+
+  irrecv.enableIRIn();
+
+  turnOffAllLeds();
+  cycleThroughLeds();
 }
 
 void loop() {
-  val = digitalRead(inPin);
-  if (val == HIGH) {
-    digitalWrite(ledGreenPin, LOW);
-    digitalWrite(ledRedPin, HIGH);
-    digitalWrite(motorLeftForwardPin, HIGH);
-    digitalWrite(motorLeftReversePin, LOW);
-    digitalWrite(motorRightForwardPin, HIGH);
-    digitalWrite(motorRightReversePin, LOW);
-  } else {
-    digitalWrite(ledGreenPin, HIGH);
-    digitalWrite(ledRedPin, LOW);
-    digitalWrite(motorLeftForwardPin, LOW);
-    digitalWrite(motorLeftReversePin, HIGH);
-    digitalWrite(motorRightForwardPin, LOW);
-    digitalWrite(motorRightReversePin, HIGH);
+  if (irrecv.decode(&results)) {
+    toggleOn(ledBluePin);
+    Serial.println(results.value, HEX);
+    irrecv.resume();
+    delay(100);
+    toggleOn(ledGreenPin);
   }
+
+  switch(results.value) {
+    case 0xFF02FD:
+      if (isMoving) {
+        stop();
+      } else {
+        moveForward(5);
+      }
+      break;
+  }
+}
+
+void turnOffAllLeds() {
+  Serial.println("Turning off all leds...");
+  for (int i = 0; i < NUMBER_OF_LEDS; i++) {
+    turnOff(leds[i]);
+  }
+}
+
+void cycleThroughLeds() {
+  Serial.println("Cycling through all leds...");
+  for (int i = 0; i < NUMBER_OF_LEDS; i++) {
+    turnOn(leds[i]);
+    delay(200);
+    turnOff(leds[i]);
+  }
+}
+
+void toggleOn(int pin) {
+  turnOffAllLeds();
+
+  Serial.println("Toggling on led...");
+  
+  for (int i = 0; i < NUMBER_OF_LEDS; i++) {
+    if(pin == leds[i]) {
+      turnOn(leds[i]);
+    }
+  }
+}
+
+void turnOn(int pin) {
+  // Serial.println("Turning on led...");
+  digitalWrite(pin, HIGH);
+}
+
+void turnOff(int pin) {
+  // Serial.println("Turning off led...");
+  digitalWrite(pin, LOW);
+}
+
+void turnLeft(int speed) {
+  isMoving = true;
+  Serial.println("Turning left...");
+  int actualSpeed = convertSpeed(speed);
+  motorLeft.forward(actualSpeed);
+  motorRight.back(actualSpeed);
+}
+
+void turnRight(int speed) {
+  isMoving = true;
+  Serial.println("Turning right...");
+  int actualSpeed = convertSpeed(speed);
+  motorLeft.back(actualSpeed);
+  motorRight.forward(actualSpeed);
+}
+
+void moveBack(int speed) {
+  isMoving = true;
+  Serial.println("Reversing...");
+  int actualSpeed = convertSpeed(speed);
+  motorLeft.back(actualSpeed);
+  motorRight.back(actualSpeed);
+}
+
+void moveForward(int speed) {
+  isMoving = true;
+  Serial.println("Moving forward...");
+  int actualSpeed = convertSpeed(speed);
+  Serial.println(actualSpeed);
+  motorLeft.forward(actualSpeed);
+  motorRight.forward(actualSpeed);
+}
+
+void stop() {
+  isMoving = false;
+  Serial.println("Stopping...");
+  motorLeft.stop();
+  motorRight.stop();
+}
+
+int convertSpeed(int speed) {
+  if (speed == 0) {
+    return 0;
+  }
+  return map(speed, 1, 5, 150, 255);
 }
