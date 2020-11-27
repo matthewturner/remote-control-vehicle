a7f5a095bb741ea722d461c86dd4466837ded4ca
Matt Turner <matt.turner@ul.com>
2020-11-27 16:06:19 +0000

    Smooth out the sensor readings

--- modules/sensor/sensor.ino
+++ modules/sensor/sensor.ino
@@ -3,10 +3,14 @@
 #include <HCSR04.h>
 
 #define NUMBER_OF_SENSORS 3
+#define NUMBER_OF_READINGS 3
+#define INDEX_OF_LAST_READING NUMBER_OF_READINGS - 1
 
 HCSR04 sensors(8, new int[NUMBER_OF_SENSORS]{9, 10, 11}, NUMBER_OF_SENSORS);
 byte payload[6];
+byte readings[NUMBER_OF_SENSORS][NUMBER_OF_READINGS];
 unsigned long lastReading;
+byte readingIndex = 0;
 
 #define MAX_SENSOR_DISTANCE 250
 
@@ -34,16 +38,25 @@ void setup()
 
 void loop()
 {
-  payload[FRONT_INDEX] = min(sensors.dist(FRONT_INDEX), MAX_SENSOR_DISTANCE);
-  payload[LEFT_INDEX] = min(sensors.dist(LEFT_INDEX), MAX_SENSOR_DISTANCE);
-  payload[RIGHT_INDEX] = min(sensors.dist(RIGHT_INDEX), MAX_SENSOR_DISTANCE);
+  readFrom(FRONT_INDEX, readingIndex);
+  readFrom(LEFT_INDEX, readingIndex);
+  readFrom(RIGHT_INDEX, readingIndex);
 
-  Serial.print(payload[LEFT_INDEX]);
+  Serial.print(readings[LEFT_INDEX][readingIndex]);
   Serial.print("cm <-- ^ ");
-  Serial.print(payload[FRONT_INDEX]);
+  Serial.print(readings[FRONT_INDEX][readingIndex]);
   Serial.print("cm ^ --> ");
-  Serial.print(payload[RIGHT_INDEX]);
+  Serial.print(readings[RIGHT_INDEX][readingIndex]);
   Serial.println("cm");
+
+  if (readingIndex >= INDEX_OF_LAST_READING)
+  {
+    readingIndex = 0;
+  }
+  else
+  {
+    readingIndex++;
+  }
 
   lastReading = millis();
   delay(100);
@@ -51,8 +64,36 @@ void loop()
 
 void requestEvent()
 {
+  payload[FRONT_INDEX] = average(readings[FRONT_INDEX]);
+  payload[LEFT_INDEX] = average(readings[LEFT_INDEX]);
+  payload[RIGHT_INDEX] = average(readings[RIGHT_INDEX]);
+
   unsigned long time = millis();
   unsigned long age = min(time - lastReading, 250);
   payload[AGE_INDEX] = (byte)age;
+  
   Wire.write(payload, 6);
+}
+
+byte readFrom(byte sensorIndex, byte readingIndex)
+{
+  byte reading = min(sensors.dist(index), MAX_SENSOR_DISTANCE);
+  if (reading == 0)
+  {
+    Serial.println("Skipping reading...");
+  }
+  else
+  {
+    readings[sensorIndex][readingIndex] = reading;
+  }
+}
+
+byte average(byte readingArray[])
+{
+  int sum = 0;
+  for (byte i = 0; i < NUMBER_OF_READINGS; i++)
+  {
+    sum += readingArray[i];
+  }
+  return (byte)(sum / NUMBER_OF_READINGS);
 }
