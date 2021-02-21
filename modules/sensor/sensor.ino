4a76a7aaadf2cc8b36614d74b17b02d2f4f2e274
Matt Turner <matt@fiverings.uk>
2021-02-21 11:58:48 +0000

    Convert self-drive to use ir sensors (#3)

--- modules/sensor/sensor.ino
+++ modules/sensor/sensor.ino
@@ -1,19 +1,15 @@
 #include <Wire.h>
+#include <Arduino.h>
 
-#include <HCSR04.h>
-
-#define COLLISION_THRESHOLD 10
-#define WARNING_THRESHOLD 20
-#define PENDING_DATA_PIN 13
+#define COLLISION_THRESHOLD 8
+#define LEFT_COLLISION_PIN 10
+#define RIGHT_COLLISION_PIN 8
+#define FRONT_COLLISION_PIN 3
 
 #define NUMBER_OF_SENSORS 3
 
-HCSR04 sensors(8, new int[NUMBER_OF_SENSORS]{9, 10, 11}, NUMBER_OF_SENSORS);
 byte payload[6];
 byte readings[NUMBER_OF_SENSORS];
-unsigned long lastReading;
-
-#define MAX_SENSOR_DISTANCE 250
 
 #define FRONT_INDEX 0
 #define LEFT_INDEX 1
@@ -22,16 +18,6 @@ unsigned long lastReading;
 #define AGE_INDEX 4
 #define SENTINEL_INDEX 5
 
-#define COLLISION_INDEX 0
-#define WARNING_INDEX 1
-#define FRONT_WARNING_PIN 2
-#define FRONT_COLLISION_PIN 3
-#define RIGHT_WARNING_PIN 4
-#define RIGHT_COLLISION_PIN 5
-#define LEFT_WARNING_PIN 6
-#define LEFT_COLLISION_PIN 7
-byte indicators[NUMBER_OF_SENSORS][2];
-
 #define SIGNAL_PIN 12
 
 void setup()
@@ -39,23 +25,12 @@ void setup()
   Serial.begin(9600);
   Wire.begin(8);
 
-  pinMode(RIGHT_WARNING_PIN, OUTPUT);
-  pinMode(RIGHT_COLLISION_PIN, OUTPUT);
-  pinMode(LEFT_WARNING_PIN, OUTPUT);
-  pinMode(LEFT_COLLISION_PIN, OUTPUT);
-  pinMode(FRONT_WARNING_PIN, OUTPUT);
-  pinMode(FRONT_COLLISION_PIN, OUTPUT);
-  pinMode(PENDING_DATA_PIN, OUTPUT);
+  pinMode(RIGHT_COLLISION_PIN, INPUT);
+  pinMode(LEFT_COLLISION_PIN, INPUT);
+  pinMode(FRONT_COLLISION_PIN, INPUT);
 
   pinMode(SIGNAL_PIN, OUTPUT);
   digitalWrite(SIGNAL_PIN, LOW);
-
-  indicators[RIGHT_INDEX][WARNING_INDEX] = RIGHT_WARNING_PIN;
-  indicators[RIGHT_INDEX][COLLISION_INDEX] = RIGHT_COLLISION_PIN;
-  indicators[LEFT_INDEX][WARNING_INDEX] = LEFT_WARNING_PIN;
-  indicators[LEFT_INDEX][COLLISION_INDEX] = LEFT_COLLISION_PIN;
-  indicators[FRONT_INDEX][WARNING_INDEX] = FRONT_WARNING_PIN;
-  indicators[FRONT_INDEX][COLLISION_INDEX] = FRONT_COLLISION_PIN;
 
   payload[FRONT_INDEX] = -1;
   payload[LEFT_INDEX] = -1;
@@ -67,20 +42,38 @@ void setup()
   Wire.onRequest(requestEvent);
 }
 
+byte frontReading = 0;
+byte leftReading = 0;
+byte rightReading = 0;
+bool signal = false;
+
 void loop()
 {
-  readFrom(FRONT_INDEX);
-  readFrom(RIGHT_INDEX);
-  readFrom(LEFT_INDEX);
+  frontReading = readFrom(FRONT_COLLISION_PIN);
+  leftReading = readFrom(LEFT_COLLISION_PIN);
+  rightReading = readFrom(RIGHT_COLLISION_PIN);
 
-  // Serial.print(readings[LEFT_INDEX][readingIndex]);
-  // Serial.print("cm <-- ^ ");
-  // Serial.print(readings[FRONT_INDEX][readingIndex]);
-  // Serial.print("cm ^ --> ");
-  // Serial.print(readings[RIGHT_INDEX][readingIndex]);
-  // Serial.println("cm");
+  if (readings[FRONT_INDEX] != frontReading)
+  {
+    readings[FRONT_INDEX] = frontReading;
+    signal = true;
+  }
+  if (readings[LEFT_INDEX] != leftReading)
+  {
+    readings[LEFT_INDEX] = leftReading;
+    signal = true;
+  }
+  if (readings[RIGHT_INDEX] != rightReading)
+  {
+    readings[RIGHT_INDEX] = rightReading;
+    signal = true;
+  }
 
-  lastReading = millis();
+  if (signal)
+  {
+    digitalWrite(SIGNAL_PIN, HIGH);
+    signal = false;
+  }
 }
 
 void requestEvent()
@@ -89,51 +82,21 @@ void requestEvent()
   payload[LEFT_INDEX] = readings[LEFT_INDEX];
   payload[RIGHT_INDEX] = readings[RIGHT_INDEX];
 
-  unsigned long time = millis();
-  unsigned long age = min(time - lastReading, 250);
-  payload[AGE_INDEX] = (byte)age;
+  payload[AGE_INDEX] = 0;
 
   Wire.write(payload, 6);
 
   digitalWrite(SIGNAL_PIN, LOW);
-  digitalWrite(PENDING_DATA_PIN, LOW);
 }
 
-byte readFrom(byte sensorIndex)
+byte readFrom(byte pin)
 {
-  byte reading = min(sensors.dist(sensorIndex), MAX_SENSOR_DISTANCE);
-  if (reading == 0)
+  if (digitalRead(pin) == HIGH)
   {
-    Serial.println("Skipping reading...");
-    return;
-  }
-
-  byte previousReading = readings[sensorIndex];
-  readings[sensorIndex] = reading;
-
-  if (reading <= COLLISION_THRESHOLD)
-  {
-    digitalWrite(indicators[sensorIndex][WARNING_INDEX], LOW);
-    digitalWrite(indicators[sensorIndex][COLLISION_INDEX], HIGH);
-    if (previousReading > COLLISION_THRESHOLD)
-    {
-      digitalWrite(SIGNAL_PIN, HIGH);
-      digitalWrite(PENDING_DATA_PIN, HIGH);
-    }
-  }
-  else if (reading <= WARNING_THRESHOLD)
-  {
-    digitalWrite(indicators[sensorIndex][COLLISION_INDEX], LOW);
-    digitalWrite(indicators[sensorIndex][WARNING_INDEX], HIGH);
-    if (previousReading > WARNING_THRESHOLD)
-    {
-      digitalWrite(SIGNAL_PIN, HIGH);
-      digitalWrite(PENDING_DATA_PIN, HIGH);
-    }
+    return 100;
   }
   else
   {
-    digitalWrite(indicators[sensorIndex][WARNING_INDEX], LOW);
-    digitalWrite(indicators[sensorIndex][COLLISION_INDEX], LOW);
+    return COLLISION_THRESHOLD;
   }
 }
