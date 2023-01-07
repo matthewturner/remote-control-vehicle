f660a18344f76d770aab733ddceccbe3b6aff158
Matt Turner <matt@fiverings.uk>
2023-01-07 16:54:54 +0000

    Create first test

---
+++ test/AutoPilotModule/test_auto_pilot_module.cpp
@@ -0,0 +1,37 @@
+#include <ArduinoFake.h>
+#include <unity.h>
+
+#include "AutoPilotModule.h"
+
+using namespace fakeit;
+
+Stream *stream;
+AutoPilotModule *target;
+
+void setUp(void)
+{
+    ArduinoFakeReset();
+    stream = ArduinoFakeMock(Stream);
+    target = new AutoPilotModule(stream, nullptr, nullptr);
+}
+
+void tearDown(void)
+{
+    delete target;
+    delete stream;
+}
+
+void test_does_nothing_when_disabled(void)
+{
+    TEST_ASSERT_TRUE(true);
+}
+
+int main(int argc, char **argv)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_does_nothing_when_disabled);
+
+    UNITY_END();
+
+    return 0;
+}
