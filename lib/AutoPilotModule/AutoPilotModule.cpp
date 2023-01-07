949168554de8e760a476f77e4b6812aa93c8778b
Matt Turner <matt@fiverings.uk>
2023-01-07 20:10:01 +0000

    Expose autopilot methods for testing

--- lib/AutoPilotModule/AutoPilotModule.cpp
+++ lib/AutoPilotModule/AutoPilotModule.cpp
@@ -3,7 +3,7 @@
 
 AutoPilotModule::AutoPilotModule(Stream *stream,
                                  IDrivingModule *drivingModule,
-                                 SensorModule *sensorModule)
+                                 ISensorModule *sensorModule)
 {
     _stream = stream;
     _drivingModule = drivingModule;
@@ -34,45 +34,23 @@ void AutoPilotModule::handle()
         return;
     }
 
-    SensorResult *sensorResult = &_sensorResult;
+    updatePositionIfRequired();
 
-    unsigned long sensorResultAge = millis() - _sampleAge + sensorResult->Age;
-
-    if (_sensorModule->signalled() || (sensorResultAge > _maxSensorResultAge))
-    {
-        _drivingModule->stop();
-
-        _sensorModule->detect(sensorResult);
-
-        _sampleAge = millis();
-
-        debugPrintln("Sensor Module Result:");
-        debugPrint("   ");
-        debugPrintln(sensorResult->Front);
-        debugPrint(sensorResult->Left);
-        debugPrint("       ");
-        debugPrintln(sensorResult->Right);
-        debugPrint("   ");
-        debugPrintln(sensorResult->Back);
-        debugPrint("   ~");
-        debugPrintln(sensorResult->Age);
-    }
-
-    if (isTrapped(sensorResult))
+    if (isTrapped())
     {
         _drivingModule->stop();
         return;
     }
 
-    if (!spaceAhead(sensorResult))
+    if (!spaceAhead())
     {
-        if (sensorResult->Right > sensorResult->Left)
+        if (_sensorResult.Right > _sensorResult.Left)
         {
             _maxSensorResultAge = MAX_SENSOR_AGE_FOR_TURN;
             _drivingModule->turnRight();
             return;
         }
-        if (sensorResult->Left > sensorResult->Right)
+        if (_sensorResult.Left > _sensorResult.Right)
         {
             _maxSensorResultAge = MAX_SENSOR_AGE_FOR_TURN;
             _drivingModule->turnLeft();
@@ -84,94 +62,129 @@ void AutoPilotModule::handle()
         return;
     }
 
-    if (sensorResult->Left < SIDE_SENSOR_COLLISION_THRESHOLD)
+    if (_sensorResult.Left < SIDE_SENSOR_COLLISION_THRESHOLD)
     {
         _maxSensorResultAge = MAX_SENSOR_AGE_FOR_TURN;
         _drivingModule->turnRight();
         return;
     }
-    if (sensorResult->Right < SIDE_SENSOR_COLLISION_THRESHOLD)
+    if (_sensorResult.Right < SIDE_SENSOR_COLLISION_THRESHOLD)
     {
         _maxSensorResultAge = MAX_SENSOR_AGE_FOR_TURN;
         _drivingModule->turnLeft();
         return;
     }
-    if (sensorResult->Left < SIDE_SENSOR_COLLISION_WARNING_THRESHOLD)
+    if (_sensorResult.Left < SIDE_SENSOR_COLLISION_WARNING_THRESHOLD)
     {
-        _maxSensorResultAge = sensorResult->Left * MAX_SENSOR_AGE_MULTIPLIER_FOR_BEAR;
+        _maxSensorResultAge = _sensorResult.Left * MAX_SENSOR_AGE_MULTIPLIER_FOR_BEAR;
         _drivingModule->bearRight(DIR_FORWARD);
         return;
     }
-    if (sensorResult->Right < SIDE_SENSOR_COLLISION_WARNING_THRESHOLD)
+    if (_sensorResult.Right < SIDE_SENSOR_COLLISION_WARNING_THRESHOLD)
     {
-        _maxSensorResultAge = sensorResult->Right * MAX_SENSOR_AGE_MULTIPLIER_FOR_BEAR;
+        _maxSensorResultAge = _sensorResult.Right * MAX_SENSOR_AGE_MULTIPLIER_FOR_BEAR;
         _drivingModule->bearLeft(DIR_FORWARD);
         return;
     }
-    if (isCentered(sensorResult))
+    if (isCentered())
     {
-        _maxSensorResultAge = sensorResult->Front * MAX_SENSOR_AGE_MULTIPLIER_FOR_FORWARD;
+        _maxSensorResultAge = _sensorResult.Front * MAX_SENSOR_AGE_MULTIPLIER_FOR_FORWARD;
         _drivingModule->moveForward();
         return;
     }
-    if (isOneSideClear(sensorResult))
+    if (isOneSideClear())
     {
-        _maxSensorResultAge = sensorResult->Front * MAX_SENSOR_AGE_MULTIPLIER_FOR_FORWARD;
+        _maxSensorResultAge = _sensorResult.Front * MAX_SENSOR_AGE_MULTIPLIER_FOR_FORWARD;
         _drivingModule->moveForward();
         return;
     }
-    if (sensorResult->Left < sensorResult->Right)
+    if (_sensorResult.Left < _sensorResult.Right)
     {
-        _maxSensorResultAge = sensorResult->Left * MAX_SENSOR_AGE_MULTIPLIER_FOR_BEAR;
+        _maxSensorResultAge = _sensorResult.Left * MAX_SENSOR_AGE_MULTIPLIER_FOR_BEAR;
         _drivingModule->bearRight(DIR_FORWARD);
         return;
     }
     else
     {
-        _maxSensorResultAge = sensorResult->Right * MAX_SENSOR_AGE_MULTIPLIER_FOR_BEAR;
+        _maxSensorResultAge = _sensorResult.Right * MAX_SENSOR_AGE_MULTIPLIER_FOR_BEAR;
         _drivingModule->bearLeft(DIR_FORWARD);
         return;
     }
 }
 
-bool AutoPilotModule::isCentered(SensorResult *sensorResult)
+unsigned long AutoPilotModule::sampleAge()
 {
-    if (sensorResult->Left - CENTER_TOLERANCE < sensorResult->Right && sensorResult->Right < sensorResult->Left + CENTER_TOLERANCE)
+    return _sampleAge;
+}
+
+unsigned int AutoPilotModule::maxSensorResultAge()
+{
+    return _maxSensorResultAge;
+}
+
+bool AutoPilotModule::updatePositionIfRequired()
+{
+    unsigned long sensorResultAge = millis() - _sampleAge + _sensorResult.Age;
+
+    if (_sensorModule->signalled() || (sensorResultAge > _maxSensorResultAge))
+    {
+        _drivingModule->stop();
+
+        _sensorModule->detect(&_sensorResult);
+
+        _sampleAge = millis();
+
+        debugPrintln("Sensor Module Result:");
+        debugPrint("   ");
+        debugPrintln(sensorResult.Front);
+        debugPrint(sensorResult.Left);
+        debugPrint("       ");
+        debugPrintln(sensorResult.Right);
+        debugPrint("   ");
+        debugPrintln(sensorResult.Back);
+        debugPrint("   ~");
+        debugPrintln(sensorResult.Age);
+    }
+}
+
+bool AutoPilotModule::isCentered()
+{
+    if (_sensorResult.Left - CENTER_TOLERANCE < _sensorResult.Right && _sensorResult.Right < _sensorResult.Left + CENTER_TOLERANCE)
     {
         return true;
     }
-    if (sensorResult->Right - CENTER_TOLERANCE < sensorResult->Left && sensorResult->Left < sensorResult->Right + CENTER_TOLERANCE)
+    if (_sensorResult.Right - CENTER_TOLERANCE < _sensorResult.Left && _sensorResult.Left < _sensorResult.Right + CENTER_TOLERANCE)
     {
         return true;
     }
     return false;
 }
 
-bool AutoPilotModule::isOneSideClear(SensorResult *sensorResult)
+bool AutoPilotModule::isOneSideClear()
 {
-    if (sensorResult->Left > SIDE_SENSOR_CLEAR_THRESHOLD)
+    if (_sensorResult.Left > SIDE_SENSOR_CLEAR_THRESHOLD)
     {
         return true;
     }
-    if (sensorResult->Right > SIDE_SENSOR_CLEAR_THRESHOLD)
+    if (_sensorResult.Right > SIDE_SENSOR_CLEAR_THRESHOLD)
     {
         return true;
     }
     return false;
 }
 
-bool AutoPilotModule::spaceAhead(SensorResult *sensorResult)
+bool AutoPilotModule::spaceAhead()
 {
-    return sensorResult->Front > FRONT_SENSOR_COLLISION_THRESHOLD;
+    return _sensorResult.Front > FRONT_SENSOR_COLLISION_THRESHOLD;
 }
 
-bool AutoPilotModule::isTrapped(SensorResult *sensorResult)
+bool AutoPilotModule::isTrapped()
 {
-    if (sensorResult->Right > SIDE_SENSOR_COLLISION_THRESHOLD)
+    if (_sensorResult.Right > SIDE_SENSOR_COLLISION_THRESHOLD)
     {
         return false;
     }
-    if (sensorResult->Left > SIDE_SENSOR_COLLISION_THRESHOLD)
+    if (_sensorResult.Left > SIDE_SENSOR_COLLISION_THRESHOLD)
     {
         return false;
     }
--- test/test_auto_pilot_module/test_auto_pilot_module.cpp
+++ test/test_auto_pilot_module/test_auto_pilot_module.cpp
@@ -7,13 +7,17 @@ using namespace fakeit;
 
 Stream *stream;
 AutoPilotModule *target;
+Mock<ISensorModule> sensorModuleMock;
+Mock<IDrivingModule> drivingModuleMock;
 
 void setUp(void)
 {
     ArduinoFakeReset();
-    // When(Method(ArduinoFake(Wire), begin)).AlwaysReturn();
+    
     stream = ArduinoFakeMock(Stream);
-    target = new AutoPilotModule(stream, nullptr, nullptr);
+    ISensorModule &sensorModule = sensorModuleMock.get();
+    IDrivingModule &drivingModule = drivingModuleMock.get();
+    target = new AutoPilotModule(stream, &drivingModule, &sensorModule);
 }
 
 void tearDown(void)
@@ -22,14 +26,38 @@ void tearDown(void)
     delete stream;
 }
 
+void test_disabled_by_default(void)
+{
+    TEST_ASSERT_FALSE(target->enabled());
+}
+
+void test_can_be_enabled(void)
+{
+    target->enable();
+    TEST_ASSERT_TRUE(target->enabled());
+}
+
+void test_can_be_disabled(void)
+{
+    target->enable();
+    target->disable();
+    TEST_ASSERT_FALSE(target->enabled());
+}
+
 void test_does_nothing_when_disabled(void)
 {
-    TEST_ASSERT_TRUE(true);
+    When(Method(ArduinoFake(), millis)).Return(1);
+    target->handle();
+    Verify(Method(ArduinoFake(), millis)).Never();
 }
 
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
+    
+    RUN_TEST(test_disabled_by_default);
+    RUN_TEST(test_can_be_enabled);
+    RUN_TEST(test_can_be_disabled);
     RUN_TEST(test_does_nothing_when_disabled);
 
     UNITY_END();
